5515d91a0247ecec78dcae466871633210418387
maarufvazifdar <maarufvazifdar@gmail.com>
2021-10-02 03:22:14 -0400

    check if tests pass

--- test/test.cpp
+++ test/test.cpp
@@ -3,7 +3,7 @@
  * @author Maaruf Vazifdar
  * @author Rishabh Mukund
  * @brief A class to compute PID values for a system.
- * @version 1.0
+ * @version 1.1
  * @date 09/30/2021
  * Copyright [2021] <Pair B>
  */
